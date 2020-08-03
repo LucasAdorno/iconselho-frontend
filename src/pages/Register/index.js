8b486e4993a3967d2145a5dd4fd85dbeb11aa621
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-03 03:02:36 -0300

    return login page text

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -45,7 +45,7 @@ export default function Register() {
 
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041"/>
-                        Não tenho cadastro
+                        Já estou cadastrado
                     </Link>
 
                 </section>
