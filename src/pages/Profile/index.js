64119c6b4f2acc09f7288f23a356acf7ca26562b
LucasAdorno <lucasadorno2702@gmail.com>
2020-08-04 18:22:34 -0300

    new test profile interface

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
 import { FiPower } from 'react-icons/fi';
 
@@ -10,74 +10,86 @@ import './styles.css';
 
 export default function Profile() {
 
-    const history = useHistory();
+  const history = useHistory();
+  const formSiac = useRef();
+  const userId = localStorage.getItem('userId');
+  const userName = localStorage.getItem('userName');
+  const [cpf, setCpf] = useState('');
+  const [password, setPassword] = useState('');
+  const [dados, setDados] = useState('')
 
-    const userId = localStorage.getItem('userId');
-    const userName = localStorage.getItem('userName');
-    const [cpf, setCpf] = useState('');
-    const [password, setPassword] = useState('');
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: userId,
+      }
+    }).then(response => {
+    })
+  }, [userId]);
 
-    useEffect(() => {
-        api.get('profile', {
-            headers: {
-                Authorization: userId,
-            }
-        }).then(response => {
-        })
-    }, [userId]);
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+    formSiac.current.innerHTML = '<h1> Aguarde... </h1>'
 
-        const data = {
-            cpf,
-            password,
-            userId
-        };
+    const data = {
+      cpf,
+      password,
+      userId
+    };
 
-        try {
-            const response = await api.post('profile', data);
-            
-            localStorage.setItem('dados', response.data);
-        }catch(err){
-            console.error()
+    try {
+      const response = await api.post('profile', data);
+      setDados(response.data);
+
+    } catch (err) {
+      console.error()
+    }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+
+    history.push('/');
+  }
+
+  return (
+    <div className="profile-container">
+      <header>
+
+        <span>Bem vindo(a), {userName}</span>
+
+        <button onClick={handleLogout} type="button">
+          <FiPower size={18} color="#E02041" />
+        </button>
+      </header>
+      <section className="form">
+
+        <form onSubmit={handleSubmit} ref={formSiac}>
+          <h1>Insira suas credenciais do SIAC</h1>
+
+          <input placeholder="CPF"
+            value={cpf}
+            onChange={e => setCpf(e.target.value)}
+          />
+          <input placeholder="Senha"
+            type='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <button className="button" type="submit">Entrar</button>
+        </form>
+      </section>
+      <section id='Obrigatórias'>
+        {dados.filterObg ? formSiac.current.innerHTML = ' ' : <></>}
+        {dados.filterObg ?
+          dados.validsValue.map(discipline => <div><p>{discipline.cod} {discipline.name}</p></div>)
+          :
+          <></>
         }
-    }
+      </section>
 
-    function handleLogout() {
-        localStorage.clear();
-
-        history.push('/');
-    }
-
-    return(
-        <div className="profile-container">
-            <header>
-
-                <span>Bem vindo(a), {userName}</span>
-
-                <button onClick={handleLogout} type="button">
-                    <FiPower size={18} color="#E02041"/>
-                </button>
-            </header>
-            <section className="form">
-
-                <form onSubmit={handleSubmit}>
-                    <h1>Insira suas informações do SIAC</h1>
-
-                    <input placeholder="CPF"
-                    value={cpf}
-                    onChange={e => setCpf(e.target.value)}
-                    />
-                    <input placeholder="Senha"
-                    type='password'
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    />
-
-                    <button className="button" type="submit">Entrar</button>
-                </form>
-            </section>
-        </div>
-    );
+    </div>
+  );
 }
